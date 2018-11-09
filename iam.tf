30c2d38a79c144b7c0e5efd69151a0e6a1c89fa7
Valter Douglas Lisboa Junior <valter.lisboa@cluster2go.com.br>
2018-11-09 20:58:29 -0200

    fix IAM role name

--- iam.tf
+++ iam.tf
@@ -1,7 +1,7 @@
 module "iam" {
   source = "github.com/pro-terraform-blueprints/terraform-aws-iam-role-blueprint//modules/config-iam-role?ref=0.0.3"
 
-  name_prefix    = "${var.name}"
+  name_prefix    = "config-${var.name}"
   description    = "AWS Config role to retrieve resources data and store logs on S3 Bucket"
   log_bucket_arn = "${module.log_bucket.bucket["arn"]}"
 }
