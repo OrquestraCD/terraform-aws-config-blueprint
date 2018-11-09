38e8e23ee8b5a5b817cdec1f77ae4e07790ea30e
Valter Douglas Lisboa Junior <valter.lisboa@cluster2go.com.br>
2018-11-09 20:46:00 -0200

    update IAM blueprint version

--- iam.tf
+++ iam.tf
@@ -1,5 +1,5 @@
 module "iam" {
-  source = "github.com/pro-terraform-blueprints/terraform-aws-iam-role-blueprint//modules/config-iam-role?ref=0.0.2"
+  source = "github.com/pro-terraform-blueprints/terraform-aws-iam-role-blueprint//modules/config-iam-role?ref=0.0.3"
 
   name_prefix    = "${var.name}"
   description    = "AWS Config role to retrieve resources data and store logs on S3 Bucket"
