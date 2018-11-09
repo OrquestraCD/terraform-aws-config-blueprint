358da2dbb0293aac1523fd66201824062f5d486c
Valter Douglas Lisboa Junior <valter.lisboa@cluster2go.com.br>
2018-11-09 20:31:21 -0200

    force bucket destruction on basic example

--- examples/basic/data.tf
+++ examples/basic/data.tf
@@ -1,6 +1,7 @@
 locals {
-  name_prefix = "basic-config"
-  name        = "${local.name_prefix}-${local.common_tags["Environment"]}"
+  name_prefix          = "basic-config"
+  name                 = "${local.name_prefix}-${local.common_tags["Environment"]}"
+  bucket_force_destroy = true
 
   common_tags = {
     Environment = "tst"
