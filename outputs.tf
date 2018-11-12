30f3b30d72d1c208ca7da8eaab8b551b1a031d6c
Valter Douglas Lisboa Junior <valter.lisboa@cluster2go.com.br>
2018-11-12 09:55:24 -0200

    create the outputs for configs

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,23 @@
 output "settings" {
   description = "The AWS Config settings for the account"
-  value       = "${module.config.settings}"
+
+  value = {
+    default_region = "${module.config.settings}"
+    region_0       = "${module.config_region_0.settings}"
+    region_1       = "${module.config_region_1.settings}"
+    region_2       = "${module.config_region_2.settings}"
+    region_3       = "${module.config_region_3.settings}"
+    region_4       = "${module.config_region_4.settings}"
+    region_5       = "${module.config_region_5.settings}"
+    region_6       = "${module.config_region_6.settings}"
+    region_7       = "${module.config_region_7.settings}"
+    region_8       = "${module.config_region_8.settings}"
+    region_9       = "${module.config_region_9.settings}"
+    region_10      = "${module.config_region_10.settings}"
+    region_11      = "${module.config_region_11.settings}"
+    region_12      = "${module.config_region_12.settings}"
+    region_13      = "${module.config_region_13.settings}"
+  }
 }
 
 output "log_bucket" {
